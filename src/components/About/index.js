b56c1ed43029e5f7e57185f636404e90e33214b5
everett <everett.diuguid@gmail.com>
2020-12-19 21:56:26 -0500

    fixed type error

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ const About = () => {
     <Row>
       <Col xs={12} md={9} lg={6} className="aboutContainer">
         <div className="pt-2 pl-2 aboutDescription">
-          Hello there! Feel free to connect with my on linkedIn or shoot me an email at everett.diuguid@gmail.com!  Hope to hear from you soon.
+          Hello there! Feel free to connect with me on linkedIn or shoot me an email at everett.diuguid@gmail.com!  Hope to hear from you soon.
         </div>
         <div className="float-right pr-4 pb-2 aboutDescription">- Everett</div>
       </Col>
