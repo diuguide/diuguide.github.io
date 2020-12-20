587fd7db98ffa46d0d4ab8b6c3d59c3cf58d7dee
everett <everett.diuguid@gmail.com>
2020-12-19 21:54:27 -0500

    responsiveness

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Container>
+      <Container fluid>
         <Row className="firstSecion">
           <Col>
             <Row>
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,11 +4,10 @@ import Col from "react-bootstrap/Col";
 const About = () => {
   return (
     <Row>
-      <Col>
+      <Col xs={12} md={9} lg={6} className="aboutContainer">
         <div className="pt-2 pl-2 aboutDescription">
-          Web developer from Raleigh, NC. I love learning new tech and collaborating with other developers.  I have expertise developing and deploying MERN stack applications in addition to bootstrap 4, jQuery, MySQL, and RESTAPI's.  I have remote experience working with GitHub version control, connecting with other developers via Zoom and Slack.  Please take some time to look over my most recent projects feel free to connect with my on linkedIn or send me an email at everett.diuguid@gmail.com!  Hope to hear from you soon.
+          Hello there! Feel free to connect with my on linkedIn or shoot me an email at everett.diuguid@gmail.com!  Hope to hear from you soon.
         </div>
-        
         <div className="float-right pr-4 pb-2 aboutDescription">- Everett</div>
       </Col>
     </Row>
