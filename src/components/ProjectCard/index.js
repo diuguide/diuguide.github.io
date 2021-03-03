857c49e15214e127883caf915c864a1afa0098ac
diuguide <everett.diuguid@gmail.com>
2021-03-03 09:42:13 -0500

    rounded card borders, shadow card borders

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -3,13 +3,13 @@ import Button from "react-bootstrap/Button";
 
 const ProjectCard = ({ title, description, img, stack, deploy, github }) => {
   return (
-    <Card className="m-2" style={{ width: "18rem" }}>
+    <Card className="m-2 shadow border-rounded" style={{ width: "18rem" }}>
       <Card.Img variant="top" src={img} />
-      <Card.Body>
+      <Card.Body classNmae="border-rounded">
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
       </Card.Body>
-      <Card.Body>
+      <Card.Body classNmae="border-rounded">
         <Button href={deploy}>Deployment</Button>
         <Button href={github}>Repo</Button>
       </Card.Body>
