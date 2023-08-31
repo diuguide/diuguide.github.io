35b50b3dbaffab20114e8c599cba75869a8fd0db
diuguide <everett.diuguid@gmail.com>
2023-08-30 20:13:23 -0400

    removed dead apps from project data main

--- src/components/JSONdata/projectData.js
+++ src/components/JSONdata/projectData.js
@@ -1,66 +1,13 @@
-
-
 export const projectData = {
   projects: [
-    {
-      title: "ValuePal",
-      description:
-        "A Stock analysis and information tool.",
-      img: "/screenshots/screenshotvaluepal.png",
-      stack: {
-        html:
-          "https://img.shields.io/badge/HTML5-informational?style=plastic&logo=html5&logoColor=33415c&color=979dac",
-        css:
-          "https://img.shields.io/badge/CSS3-informational?style=plastic&logo=css3&logoColor=33415c&color=979dac",
-        javaScript:
-          "https://img.shields.io/badge/JavaScript-informational?style=plastic&logo=javascript&logoColor=33415c&color=979dac",
-        bootstrap:
-          "https://img.shields.io/badge/Bootstrap-informational?style=plastic&logo=bootstrap&logoColor=33415c&color=979dac",
-        jQuery:
-          "https://img.shields.io/badge/jQuery-informational?style=plastic&logo=jquery&logoColor=33415c&color=979dac",
-        express:
-          "https://img.shields.io/badge/Express-informational?style=plastic&logo=expressjs&logoColor=33415c&color=979dac",
-        mySQL:
-          "https://img.shields.io/badge/mysql-informational?style=plastic&logo=mysql&logoColor=white&color=979dac",
-        nodeJS:
-          "https://img.shields.io/badge/NodeJS-informational?style=plastic&logo=nodejs&logoColor=33415c&color=979dac",
-      },
-      links: {
-        github: "https://github.com/diuguide/valuePal",
-        deploy: "https://valuepal.herokuapp.com/",
-      },
-    },
-    {
-      title: "Mars Rover Updates",
-      description: "A site showcasing NASA's Mars Rover Perserverance!",
-      img: "/screenshots/screen_mars.png",
-      stack: {
-        html:
-          "https://img.shields.io/badge/HTML5-informational?style=plastic&logo=html5&logoColor=33415c&color=979dac",
-        css:
-          "https://img.shields.io/badge/CSS3-informational?style=plastic&logo=css3&logoColor=33415c&color=979dac",
-        javaScript:
-          "https://img.shields.io/badge/JavaScript-informational?style=plastic&logo=javascript&logoColor=33415c&color=979dac",
-        bootstrap:
-          "https://img.shields.io/badge/Bootstrap-informational?style=plastic&logo=bootstrap&logoColor=33415c&color=979dac",
-        jQuery:
-          "https://img.shields.io/badge/jQuery-informational?style=plastic&logo=jquery&logoColor=33415c&color=979dac",
-      },
-      links: {
-        github: "https://github.com/diuguide/marsRover_updates",
-        deploy: "https://mars-rover-update.herokuapp.com/",
-      },
-    },
     {
       title: "CSS Color Grid",
       description:
         "A simple css/JS application altering the color and shape of the grid components",
       img: "/screenshots/screenshot_cssColorGrid.png",
       stack: {
-        html:
-          "https://img.shields.io/badge/HTML5-informational?style=plastic&logo=html5&logoColor=33415c&color=979dac",
-        css:
-          "https://img.shields.io/badge/CSS3-informational?style=plastic&logo=css3&logoColor=33415c&color=979dac",
+        html: "https://img.shields.io/badge/HTML5-informational?style=plastic&logo=html5&logoColor=33415c&color=979dac",
+        css: "https://img.shields.io/badge/CSS3-informational?style=plastic&logo=css3&logoColor=33415c&color=979dac",
         javaScript:
           "https://img.shields.io/badge/JavaScript-informational?style=plastic&logo=javascript&logoColor=33415c&color=979dac",
         bootstrap:
@@ -79,10 +26,8 @@ export const projectData = {
         "A search application drawing data from 3rd party APIs populating local business data along side area COVID19 data",
       img: "/screenshots/screen_isitopenNEW.png",
       stack: {
-        html:
-          "https://img.shields.io/badge/HTML5-informational?style=plastic&logo=html5&logoColor=33415c&color=979dac",
-        css:
-          "https://img.shields.io/badge/CSS3-informational?style=plastic&logo=css3&logoColor=33415c&color=979dac",
+        html: "https://img.shields.io/badge/HTML5-informational?style=plastic&logo=html5&logoColor=33415c&color=979dac",
+        css: "https://img.shields.io/badge/CSS3-informational?style=plastic&logo=css3&logoColor=33415c&color=979dac",
         javaScript:
           "https://img.shields.io/badge/JavaScript-informational?style=plastic&logo=javascript&logoColor=33415c&color=979dac",
         bootstrap:
@@ -95,97 +40,13 @@ export const projectData = {
         deploy: "https://diuguide.github.io/is_it_open/",
       },
     },
-    {
-      title: "Rain or Shine 919",
-      description:
-        "A MERN stack application that populates current and forecast weather data.  Allows users to create individualized settings.",
-      img: "/screenshots/screenshot_weathermap.png",
-      stack: {
-        html:
-          "https://img.shields.io/badge/HTML5-informational?style=plastic&logo=html5&logoColor=33415c&color=979dac",
-        css:
-          "https://img.shields.io/badge/CSS3-informational?style=plastic&logo=css3&logoColor=33415c&color=979dac",
-        javaScript:
-          "https://img.shields.io/badge/JavaScript-informational?style=plastic&logo=javascript&logoColor=33415c&color=979dac",
-        mongoDB:
-          "https://img.shields.io/badge/MongoDB-informational?style=plastic&logo=mongodb&logoColor=33415c&color=979dac",
-        express:
-          "https://img.shields.io/badge/Express-informational?style=plastic&logo=expressjs&logoColor=33415c&color=979dac",
-        react:
-          "https://img.shields.io/badge/React-informational?style=plastic&logo=react&logoColor=33415c&color=979dac",
-        nodeJS:
-          "https://img.shields.io/badge/NodeJS-informational?style=plastic&logo=nodejs&logoColor=33415c&color=979dac",
-      },
-      links: {
-        github: "https://github.com/diuguide/weather_map",
-        deploy: "http://rain-or-shine919.herokuapp.com/",
-      },
-    },
-    {
-      title: "Myriad: Symptom Tracker",
-      description:
-        "A symptom tracking application for collecting and exporting health data for doctors visits",
-      img: "/screenshots/screenshot_myriad.png",
-      stack: {
-        html:
-          "https://img.shields.io/badge/HTML5-informational?style=plastic&logo=html5&logoColor=33415c&color=979dac",
-        css:
-          "https://img.shields.io/badge/CSS3-informational?style=plastic&logo=css3&logoColor=33415c&color=979dac",
-        javaScript:
-          "https://img.shields.io/badge/JavaScript-informational?style=plastic&logo=javascript&logoColor=33415c&color=979dac",
-        bootstrap:
-          "https://img.shields.io/badge/Bootstrap-informational?style=plastic&logo=bootstrap&logoColor=33415c&color=979dac",
-        jQuery:
-          "https://img.shields.io/badge/jQuery-informational?style=plastic&logo=jquery&logoColor=33415c&color=979dac",
-        express:
-          "https://img.shields.io/badge/Express-informational?style=plastic&logo=expressjs&logoColor=33415c&color=979dac",
-        mySQL:
-          "https://img.shields.io/badge/mysql-informational?style=plastic&logo=mysql&logoColor=white&color=979dac",
-        nodeJS:
-          "https://img.shields.io/badge/NodeJS-informational?style=plastic&logo=nodejs&logoColor=33415c&color=979dac",
-      },
-      links: {
-        github: "https://github.com/diuguide/symptom_tracker",
-        deploy: "https://symptomtracker2.herokuapp.com/",
-      },
-    },
-    {
-      title: "YardPal",
-      description:
-        "MERN Stack Retail app utilizing authentication to create individualized yard sales online",
-      img: "/screenshots/screenshot_yardpal.png",
-      stack: {
-        html:
-          "https://img.shields.io/badge/HTML5-informational?style=plastic&logo=html5&logoColor=33415c&color=979dac",
-        css:
-          "https://img.shields.io/badge/CSS3-informational?style=plastic&logo=css3&logoColor=33415c&color=979dac",
-        javaScript:
-          "https://img.shields.io/badge/JavaScript-informational?style=plastic&logo=javascript&logoColor=33415c&color=979dac",
-        bootstrap:
-          "https://img.shields.io/badge/Bootstrap-informational?style=plastic&logo=bootstrap&logoColor=33415c&color=979dac",
-        mongoDB:
-          "https://img.shields.io/badge/MongoDB-informational?style=plastic&logo=mongodb&logoColor=33415c&color=979dac",
-        express:
-          "https://img.shields.io/badge/Express-informational?style=plastic&logo=expressjs&logoColor=33415c&color=979dac",
-        react:
-          "https://img.shields.io/badge/React-informational?style=plastic&logo=react&logoColor=33415c&color=979dac",
-        nodeJS:
-          "https://img.shields.io/badge/NodeJS-informational?style=plastic&logo=nodejs&logoColor=33415c&color=979dac",
-      },
-      links: {
-        github: "https://github.com/diuguide/yard-pal",
-        deploy: "https://yard-pal.herokuapp.com/",
-      },
-    },
   ],
 };
 
 export const stackData = {
   stack: {
-    html:
-      "https://img.shields.io/badge/HTML-informational?style=plastic&logo=html5&logoColor=33415c&color=979dac",
-    sass:
-      "https://img.shields.io/badge/SASS-informational?style=plastic&logo=sass&logoColor=white&color=979dac",
+    html: "https://img.shields.io/badge/HTML-informational?style=plastic&logo=html5&logoColor=33415c&color=979dac",
+    sass: "https://img.shields.io/badge/SASS-informational?style=plastic&logo=sass&logoColor=white&color=979dac",
     javaScript:
       "https://img.shields.io/badge/JavaScript-informational?style=plastic&logo=javascript&logoColor=33415c&color=979dac",
     bootstrap:
@@ -203,10 +64,8 @@ export const stackData = {
     mySQL:
       "https://img.shields.io/badge/mysql-informational?style=plastic&logo=mysql&logoColor=white&color=979dac",
 
-    java:
-      "https://img.shields.io/badge/Java-informational?style=plastic&logo=java&logoColor=white&color=979dac",
-    php:
-      "https://img.shields.io/badge/PHP-informational?style=plastic&logo=php&logoColor=white&color=979dac",
+    java: "https://img.shields.io/badge/Java-informational?style=plastic&logo=java&logoColor=white&color=979dac",
+    php: "https://img.shields.io/badge/PHP-informational?style=plastic&logo=php&logoColor=white&color=979dac",
     python:
       "https://img.shields.io/badge/Python-informational?style=plastic&logo=python&logoColor=white&color=979dac",
     github:
@@ -215,4 +74,3 @@ export const stackData = {
       "https://img.shields.io/badge/Heroku-informational?style=plastic&logo=heroku&logoColor=white&color=979dac",
   },
 };
-
